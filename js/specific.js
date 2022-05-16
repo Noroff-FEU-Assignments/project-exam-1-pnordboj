96a86d36b7919e0ca6212910523237a8cfab7c6a
pnordboj <91888307+pnordboj@users.noreply.github.com>
2022-05-16 09:41:24 +0200

    Added query strings

---
+++ js/specific.js
@@ -0,0 +1,29 @@
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+const html = document.querySelector(".getReview");
+
+const url = "https://patricknj.one/nordbo/wp-json/wp/v2/posts" + "/" + id;
+const image = "https://patricknj.one/nordbo/wp-json/wp/v2/media" + "/";
+
+async function getReview() {
+    try {
+        const fetchMovie = await fetch(url);
+        const movie = await fetchMovie.json();
+        const featureId = `${movies[i].featured_media}`;
+        const fetchImage = await fetch(image + featureId);
+        const image = await fetchImage.json();
+        html.innerHTML = `
+        <div class="specificReview">
+            <h2 id="specificTitle">${movie.title.rendered}</h3>
+            <img id="specificImg" src="${image.source_url}">
+        </div>
+        `;
+    } catch(error) {
+        console.log(error);
+        html.innerHTML = displayError('error', error);
+    }
+}
+
+getReview();
