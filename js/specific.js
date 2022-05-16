14ff337a61d198b68226d4987a10880ab95912e8
pnordboj <91888307+pnordboj@users.noreply.github.com>
2022-05-16 09:59:12 +0200

    test

--- js/specific.js
+++ js/specific.js
@@ -5,7 +5,7 @@ const id = params.get("id");
 const html = document.querySelector(".getReview");
 
 const url = "https://patricknj.one/nordbo/wp-json/wp/v2/posts" + "?" + "id=" + id;
-const image = "https://patricknj.one/nordbo/wp-json/wp/v2/media" + "/";
+const image = "https://patricknj.one/nordbo/wp-json/wp/v2/media" + "?";
 
 async function getReview() {
     try {
@@ -14,10 +14,11 @@ async function getReview() {
         const featureId = `${movie.featured_media}`;
         const fetchImage = await fetch(image + featureId);
         const image = await fetchImage.json();
+        html.innerHTML = ``;
         console.log(movie);
         html.innerHTML = `
         <div class="specificReview">
-            <h2 id="specificTitle">${movie.title.rendered}</h3>
+            <h2 id="specificTitle">${movie.title.rendered}</h2>
             <img id="specificImg" src="${image.source_url}">
         </div>
         `;
